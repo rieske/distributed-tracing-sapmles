862832a53d64fb0fb4d4a27f57accd7ce1f89ed1
Vaidotas Valuckas <vaidotas.valuckas@gmail.com>
2018-10-07 00:05:04 +0300

    Added Kafka producer

--- amqp-consumer-kafka-producer/src/main/java/lt/rieske/tracing/MessageListener.java
+++ amqp-consumer-kafka-producer/src/main/java/lt/rieske/tracing/MessageListener.java
@@ -1,15 +1,23 @@
 package lt.rieske.tracing;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 class MessageListener {
 
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
     @RabbitListener(queues = "sampleQueue")
-    public void processMessage(byte[] content) {
-        log.info("Received message: {}", new String(content));
+    void processMessage(byte[] content) {
+        var contentStr = new String(content);
+        log.info("Received message: {}", new String(contentStr));
+
+        kafkaTemplate.send("sample-topic", "some-key", contentStr);
     }
 }
